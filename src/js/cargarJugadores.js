eb4e9054d053c6427aa318f721c44be30b4c7d12
Gabbrieh <matu.g.fernandez@gmail.com>
2025-06-04 18:44:23 -0300

    Actualización de jugadores y equipos

--- src/js/cargarJugadores.js
+++ src/js/cargarJugadores.js
@@ -9,22 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
         card.classList.add("jugador_card");
         card.setAttribute("data-posicion", jugador.posicion);
 
-        card.innerHTML = `
-          <div class="jugador_fondo">
-            <div class="jugador_foto_container">
-              <img class="jugador_foto" src="${jugador.foto}" alt="${jugador.nombre}" />
-              <div class="jugador_dorsal">${jugador.dorsal}</div>
-              <div class="jugador_barra">
-                <div class="jugador_club">
-                  <img src="${jugador.clubIcon}" alt="${jugador.club}" class="club_icon" />
-                  <span>${jugador.club}</span>
-                </div>
-                <div class="jugador_posicion">${jugador.posicion}</div>
-              </div>
-              <div class="jugador_nombre">${jugador.nombre}</div>
-            </div>
-          </div>
-        `;
+card.innerHTML = `
+  <div class="jugador_fondo">
+    <div class="jugador_foto_container">
+      <img class="jugador_foto" src="${jugador.foto}" alt="${jugador.nombre}" />
+      <div class="jugador_dorsal">${jugador.dorsal}</div>
+      <div class="jugador_barra">
+        <div class="jugador_club">
+          <img src="${jugador.clubIcon}" alt="${jugador.club}" class="club_icon" />
+          <span>${jugador.club}</span>
+        </div>
+        <div class="jugador_posicion">${jugador.posicion}</div>
+        <div class="jugador_nacionalidad">
+          <img src="${jugador.nacionalidadIcon}" alt="${jugador.nacionalidad}" class="nacionalidad_icon" />
+          <span>${jugador.nacionalidad}</span>
+        </div>
+      </div>
+      <div class="jugador_nombre">${jugador.nombre}</div>
+    </div>
+  </div>
+`;
 
         contenedor.appendChild(card);
       });
